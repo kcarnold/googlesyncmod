c67b0dfc4b3d37d98674a17c8c9893d623f0b9b3
cybdis <cybdis@1126ad72-2f51-4d33-8cd1-f97aedab7a97>
2014-08-27 06:52:13 +0000

    Fixed UI design of dialogs: - Icon - Button position - Sizable / minimum size - Control layout

--- GoogleContactsSync/AddEditProfile.Designer.cs
+++ GoogleContactsSync/AddEditProfile.Designer.cs
@@ -28,83 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.bbOk = new System.Windows.Forms.Button();
-            this.btCancel = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.tbProfileName = new System.Windows.Forms.TextBox();
-            this.groupBox1.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // bbOk
-            // 
-            this.bbOk.AutoEllipsis = true;
-            this.bbOk.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.bbOk.Location = new System.Drawing.Point(21, 63);
-            this.bbOk.Name = "bbOk";
-            this.bbOk.Size = new System.Drawing.Size(75, 23);
-            this.bbOk.TabIndex = 0;
-            this.bbOk.Text = "OK";
-            this.bbOk.UseVisualStyleBackColor = true;
-            // 
-            // btCancel
-            // 
-            this.btCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btCancel.Location = new System.Drawing.Point(234, 63);
-            this.btCancel.Name = "btCancel";
-            this.btCancel.Size = new System.Drawing.Size(75, 23);
-            this.btCancel.TabIndex = 1;
-            this.btCancel.Text = "Cancel";
-            this.btCancel.UseVisualStyleBackColor = true;
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.tbProfileName);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(313, 45);
-            this.groupBox1.TabIndex = 4;
-            this.groupBox1.TabStop = false;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 16);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(68, 13);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Profile name:";
-            // 
-            // tbProfileName
-            // 
-            this.tbProfileName.Location = new System.Drawing.Point(80, 13);
-            this.tbProfileName.Name = "tbProfileName";
-            this.tbProfileName.Size = new System.Drawing.Size(217, 20);
-            this.tbProfileName.TabIndex = 4;
-            // 
-            // AddEditProfileForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(336, 94);
-            this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.btCancel);
-            this.Controls.Add(this.bbOk);
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Name = "AddEditProfileForm";
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
-            this.ResumeLayout(false);
+			this.bbOk = new System.Windows.Forms.Button();
+			this.btCancel = new System.Windows.Forms.Button();
+			this.tbProfileName = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// bbOk
+			// 
+			this.bbOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.bbOk.AutoEllipsis = true;
+			this.bbOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+			this.bbOk.Location = new System.Drawing.Point(168, 59);
+			this.bbOk.Name = "bbOk";
+			this.bbOk.Size = new System.Drawing.Size(75, 23);
+			this.bbOk.TabIndex = 2;
+			this.bbOk.Text = "OK";
+			this.bbOk.UseVisualStyleBackColor = true;
+			// 
+			// btCancel
+			// 
+			this.btCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.btCancel.Location = new System.Drawing.Point(249, 59);
+			this.btCancel.Name = "btCancel";
+			this.btCancel.Size = new System.Drawing.Size(75, 23);
+			this.btCancel.TabIndex = 3;
+			this.btCancel.Text = "Cancel";
+			this.btCancel.UseVisualStyleBackColor = true;
+			// 
+			// tbProfileName
+			// 
+			this.tbProfileName.Location = new System.Drawing.Point(15, 25);
+			this.tbProfileName.Name = "tbProfileName";
+			this.tbProfileName.Size = new System.Drawing.Size(309, 20);
+			this.tbProfileName.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(68, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Profile name:";
+			// 
+			// AddEditProfileForm
+			// 
+			this.AcceptButton = this.bbOk;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.CancelButton = this.btCancel;
+			this.ClientSize = new System.Drawing.Size(336, 94);
+			this.ControlBox = false;
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.tbProfileName);
+			this.Controls.Add(this.btCancel);
+			this.Controls.Add(this.bbOk);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Name = "AddEditProfileForm";
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "Add/Edit Profile";
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button bbOk;
-        private System.Windows.Forms.Button btCancel;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox tbProfileName;
+		private System.Windows.Forms.Button btCancel;
+		private System.Windows.Forms.TextBox tbProfileName;
+		private System.Windows.Forms.Label label1;
     }
 }
