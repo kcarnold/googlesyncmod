2bcad0117d642364522e06bb263ac1ae170370cc
big-r <big-r@1126ad72-2f51-4d33-8cd1-f97aedab7a97>
2014-11-11 12:09:22 +0000

    #GCSM - fixed newline spelling in Error Dialog - disable Checkbox "Run program at startup" if we can't write to hive (HKCU) - Unload Outlook after version detection

--- trunk/GoogleContactsSync/Properties/AssemblyInfo.cs
+++ trunk/GoogleContactsSync/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.8.5.*")]
-[assembly: AssemblyFileVersion("3.8.5")]
+[assembly: AssemblyVersion("3.8.6.*")]
+[assembly: AssemblyFileVersion("3.8.6-BETA")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
