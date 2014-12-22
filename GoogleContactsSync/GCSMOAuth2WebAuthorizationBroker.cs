30c913b4dffb1908e2c1c838016872db879de986
big-r <big-r@1126ad72-2f51-4d33-8cd1-f97aedab7a97>
2014-12-22 16:25:29 +0000

    - rename class file - small code cleanup

--- GoogleContactsSync/GCSMOAuth2WebAuthorizationBroker.cs
+++ GoogleContactsSync/GCSMOAuth2WebAuthorizationBroker.cs
@@ -9,9 +9,7 @@ using Google.Apis.Util.Store;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
