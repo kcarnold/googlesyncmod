0d17a55f0dea688ac015edbffe989f5206cb82b6
floriwan <floriwan@1126ad72-2f51-4d33-8cd1-f97aedab7a97>
2015-04-14 17:36:51 +0000

    IMPROVEMENT: Made Timezone selection a dropdown combobox to enable users to add their own timezone, if needed (e.g. America/Arizona)

--- trunk/GoogleContactsSync/SettingsForm.Designer.cs
+++ trunk/GoogleContactsSync/SettingsForm.Designer.cs
@@ -440,7 +440,6 @@ namespace GoContactSyncMod
             // 
             this.appointmentTimezonesComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.appointmentTimezonesComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.appointmentTimezonesComboBox.FormattingEnabled = true;
             this.appointmentTimezonesComboBox.Items.AddRange(new object[] {
             "Africa/Abidjan",
