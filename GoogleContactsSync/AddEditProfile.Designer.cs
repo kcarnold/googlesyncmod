2b458972ad050e8371ccc6c2eeebb3aee55d95dd
big-r <big-r@1126ad72-2f51-4d33-8cd1-f97aedab7a97>
2015-06-13 07:51:18 +0000

    - **FIX**: [bugs:#731] - **UPDATE**: nuget packages - dont load old registry settings to avoid profiel errors

--- GoogleContactsSync/AddEditProfile.Designer.cs
+++ GoogleContactsSync/AddEditProfile.Designer.cs
@@ -28,70 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-			this.bbOk = new System.Windows.Forms.Button();
-			this.btCancel = new System.Windows.Forms.Button();
-			this.tbProfileName = new System.Windows.Forms.TextBox();
-			this.label1 = new System.Windows.Forms.Label();
-			this.SuspendLayout();
-			// 
-			// bbOk
-			// 
-			this.bbOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.bbOk.AutoEllipsis = true;
-			this.bbOk.DialogResult = System.Windows.Forms.DialogResult.OK;
-			this.bbOk.Location = new System.Drawing.Point(168, 59);
-			this.bbOk.Name = "bbOk";
-			this.bbOk.Size = new System.Drawing.Size(75, 23);
-			this.bbOk.TabIndex = 2;
-			this.bbOk.Text = "OK";
-			this.bbOk.UseVisualStyleBackColor = true;
-			// 
-			// btCancel
-			// 
-			this.btCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.btCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.btCancel.Location = new System.Drawing.Point(249, 59);
-			this.btCancel.Name = "btCancel";
-			this.btCancel.Size = new System.Drawing.Size(75, 23);
-			this.btCancel.TabIndex = 3;
-			this.btCancel.Text = "Cancel";
-			this.btCancel.UseVisualStyleBackColor = true;
-			// 
-			// tbProfileName
-			// 
-			this.tbProfileName.Location = new System.Drawing.Point(15, 25);
-			this.tbProfileName.Name = "tbProfileName";
-			this.tbProfileName.Size = new System.Drawing.Size(309, 20);
-			this.tbProfileName.TabIndex = 1;
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(12, 9);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(68, 13);
-			this.label1.TabIndex = 0;
-			this.label1.Text = "Profile name:";
-			// 
-			// AddEditProfileForm
-			// 
-			this.AcceptButton = this.bbOk;
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.CancelButton = this.btCancel;
-			this.ClientSize = new System.Drawing.Size(336, 94);
-			this.ControlBox = false;
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.tbProfileName);
-			this.Controls.Add(this.btCancel);
-			this.Controls.Add(this.bbOk);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-			this.Name = "AddEditProfileForm";
-			this.ShowInTaskbar = false;
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-			this.Text = "Add/Edit Profile";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.bbOk = new System.Windows.Forms.Button();
+            this.btCancel = new System.Windows.Forms.Button();
+            this.tbProfileName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bbOk
+            // 
+            this.bbOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.bbOk.AutoEllipsis = true;
+            this.bbOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.bbOk.Location = new System.Drawing.Point(168, 51);
+            this.bbOk.Name = "bbOk";
+            this.bbOk.Size = new System.Drawing.Size(75, 26);
+            this.bbOk.TabIndex = 2;
+            this.bbOk.Text = "OK";
+            this.bbOk.UseVisualStyleBackColor = true;
+            // 
+            // btCancel
+            // 
+            this.btCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btCancel.Location = new System.Drawing.Point(249, 51);
+            this.btCancel.Name = "btCancel";
+            this.btCancel.Size = new System.Drawing.Size(75, 26);
+            this.btCancel.TabIndex = 3;
+            this.btCancel.Text = "Cancel";
+            this.btCancel.UseVisualStyleBackColor = true;
+            // 
+            // tbProfileName
+            // 
+            this.tbProfileName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbProfileName.Location = new System.Drawing.Point(12, 25);
+            this.tbProfileName.Name = "tbProfileName";
+            this.tbProfileName.Size = new System.Drawing.Size(312, 20);
+            this.tbProfileName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Profile name:";
+            // 
+            // AddEditProfileForm
+            // 
+            this.AcceptButton = this.bbOk;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.CancelButton = this.btCancel;
+            this.ClientSize = new System.Drawing.Size(336, 90);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbProfileName);
+            this.Controls.Add(this.btCancel);
+            this.Controls.Add(this.bbOk);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "AddEditProfileForm";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Add/Edit Profile";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
