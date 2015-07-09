ad3b528138fbfa28815bcf9b4fffdefe2fc2d095
floriwan <floriwan@1126ad72-2f51-4d33-8cd1-f97aedab7a97>
2015-07-09 09:44:46 +0000

    FIX: Workaround, to not overwrite tentative/free Calendar items

--- GoogleContactsSync/Properties/AssemblyInfo.cs
+++ GoogleContactsSync/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.9.10.*")]
-[assembly: AssemblyFileVersion("3.9.10")]
+[assembly: AssemblyVersion("3.9.11.*")]
+[assembly: AssemblyFileVersion("3.9.11")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
